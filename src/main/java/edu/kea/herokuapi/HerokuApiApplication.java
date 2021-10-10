7ea084e2bbe2b9ee33202cb95a840f2f696cdd02
dwaGithub <78429825+dwaGithub@users.noreply.github.com>
2021-10-10 15:05:04 +0200

    Final push

--- src/main/java/edu/kea/herokuapi/HerokuApiApplication.java
+++ src/main/java/edu/kea/herokuapi/HerokuApiApplication.java
@@ -2,10 +2,8 @@ package edu.kea.herokuapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class HerokuApiApplication {
 
     public static void main(String[] args) {
